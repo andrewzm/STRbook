7529c1eba17b1f72422a14abbcbc0884106a4155
andrewzm <andrewzm@gmail.com>
2018-11-19 03:22:37 +1100

    added quadInd to setParsEESN; made tau = tauEmb in main function

--- R/E_QESN_SST_RFunctions.R
+++ R/E_QESN_SST_RFunctions.R
@@ -1,7 +1,7 @@
 #######################################################
-################### File of functions 
+################### File of functions
 #######################################################
-#### File reads all in R functions 
+#### File reads all in R functions
 #### needed for the E-QESN SST example
 #######################################################
 #######################################################
@@ -11,65 +11,65 @@
 ############### Create Embed Data ##########################
 ############################################################
 #' @export
-createEmbedRNNData=function(m,tauEmb,yTrain,rawDataInput,curXTestIndex){
-  #Note the parameter "tau" is defined globally and therefore does not need
-  # to be directly passed to this function
+createEmbedRNNData=function(m,tau,yTrain,rawDataInput,curXTestIndex){
 
-  
+  tauEmb <- tau # force embedding length time to be forecast lead time --
+                # this can be changed in a future revision
+
   curTrainLen <- nrow(yTrain)
   numLocs <- ncol(yTrain)
   lenInSampleEmb=curTrainLen-(m*tauEmb)
   curTestLen <- length(curXTestIndex)
-  
+
   altInSampleXRaw=array(NA,c(lenInSampleEmb,m+1,numLocs))
-  
+
   for(i in 1:lenInSampleEmb){
     #####using laplacian eigenmaps
     altInSampleXRaw[i,,]=rawDataInput[seq(i,(m*tauEmb+i),by=tauEmb),]
   }
-  
+
   #######Scale in-sample x and y
   altYInSampRaw=yTrain[(m*tauEmb+1):curTrainLen,]
   altYMean=mean(altYInSampRaw)
   altScaleFactor=sd(altYInSampRaw)
-  
+
   altYInSamp=(altYInSampRaw-altYMean)/altScaleFactor
-  
-  
+
+
   meanXTrainMatrix=mean(rawDataInput[1:curTrainLen,])
   sdXTrainMatrix=sd(rawDataInput[1:curTrainLen,])
-  
-  
+
+
   altInSampleX=(altInSampleXRaw-meanXTrainMatrix)/sdXTrainMatrix
-  
-  
+
+
   designMatrix=matrix(1,lenInSampleEmb,(m+1)*numLocs+1)
   for(i in 1:lenInSampleEmb){
     designMatrix[i,2:((m+1)*numLocs+1)]=as.vector(altInSampleX[i,,])
   }
-  
-  
+
+
   ####### Out Sample
   allOutSampIndexes=(curXTestIndex[1]-tau+1):curXTestIndex[length(curXTestIndex)]
-  
+
   xMatchIndexes=match(xTestIndex,allOutSampIndexes)
 
   altOutSampleXRaw=array(NA,c(length(allOutSampIndexes),m+1,numLocs))
   for(i in 1:length(allOutSampIndexes)){
     altOutSampleXRaw[i,,]=rawDataInput[seq(allOutSampIndexes[i]-(m*tauEmb),allOutSampIndexes[i],by=tauEmb),]
   }
-  
+
   #######Scale in-sample x and y
   altOutSampleX=(altOutSampleXRaw-meanXTrainMatrix)/sdXTrainMatrix
-  
+
   designMatrixOutSample=matrix(1,length(allOutSampIndexes),(m+1)*numLocs+1)
   for(i in 1:length(allOutSampIndexes)){
     designMatrixOutSample[i,2:((m+1)*numLocs+1)]=as.vector(altOutSampleX[i,,])
   }
-  
+
   return(list(curInSampMean=altYMean,curInSampSD=altScaleFactor,curYInSamp=altYInSamp,curInSampX=altInSampleX,curOutSampX=altOutSampleX,lenInSampleEmb=lenInSampleEmb,designMatrix=designMatrix,designMatrixOutSample=designMatrixOutSample,curTestLen=curTestLen,xMatchIndexes=xMatchIndexes ))
-  
-  
+
+
 }
 
 
@@ -77,18 +77,16 @@ createEmbedRNNData=function(m,tauEmb,yTrain,rawDataInput,curXTestIndex){
 ############### Ensembel ESN Set Parameters ####################
 ################################################################
 #' @export
-setParsEESN=function(regPar,nh,numLocs,m){
-  #Note the parameter "quadInd" is defined globally and therefore does not need
-  # to be directly passed to this function
-  
+setParsEESN=function(regPar,nh,numLocs,m, quadInd){
+
   inSize = numLocs*(m+1)
   nColsU=inSize+1
-  
+
   #########sampleVec
   sampVecESN=0:(nh-1)
-  
+
   strValuesXtemp=rep(0,nh)
-  
+
   #####create Ridge Matrix
   if(quadInd){
     ridgeMat=regPar*diag(2*nh )
@@ -105,79 +103,79 @@ setParsEESN=function(regPar,nh,numLocs,m){
 ############### genResR ########################################
 ################################################################
 ################################################################
-###### Generates a single reservoir 
+###### Generates a single reservoir
 
 #' @export
 genResR=function(nh,wWidth,uWidth,piW,piU,nuESN,quadInd,DataObj,setParObj,testLen){
-  
+
   echoTrainLen=DataObj$lenInSampleEmb
-  designMat=DataObj$designMatrix 
+  designMat=DataObj$designMatrix
   designMatrixOutSample=DataObj$designMatrixOutSample
-  startValuesHMat=setParObj$strValuesXtemp 
+  startValuesHMat=setParObj$strValuesXtemp
   ridgeMat=setParObj$ridgeMat
-  scaledInSampY=DataObj$curYInSamp 
-  nColsU=ncol(designMat)  
-  inSampMean=DataObj$curInSampMean 
+  scaledInSampY=DataObj$curYInSamp
+  nColsU=ncol(designMat)
+  inSampMean=DataObj$curInSampMean
   inSampStDev=DataObj$curInSampSD
   xMatchIndexes=DataObj$xMatchIndexes
   # DataObj,setParObj
-  
+
   U=matrix(runif(nh*nColsU,-uWidth,uWidth),nh)
   W=matrix(runif(nh*nh,-wWidth,wWidth),nh)
-  
-  
+
+
   ######## Make unscaled W matrix sparse
   for(i in 1:nh){
     numNonZeroW=rbinom(1,nh,piW)
     numZeroW=nh-numNonZeroW
-    
+
     curWIndexZero=sample(1:nh,numZeroW)
     W[curWIndexZero,i]=0
   }
-  
-  
+
+
   ######## Make U matrix sparse
   for(i in 1:nColsU){
     numNonZeroU=rbinom(1,nh,piU)
     numZeroU=nh-numNonZeroU
-    
+
     curUIndexZero=sample(1:nh,numZeroU)
     U[curUIndexZero,i]=0
   }
-  
+
   ######calculate the spectral radius
   spectralRadius = abs(eigen(W,only.values=TRUE)$values[1])
-  
+
   ##### Scale the W matrix
   wMatScaled = W * nuESN / spectralRadius
-  
+
   ##### Calcaulte u Mat times design Matrix
   uProdMat=U%*%t(designMat)
-  
+
   ##### Calculate in-sample hMat
   srtValsInSamp=startValuesHMat
-  
-  
+
+
   inSampleHMatObj=createHMatR(nh,echoTrainLen,wMatScaled,uProdMat,srtValsInSamp,quadInd)
-  
+
   inSampleHMat=inSampleHMatObj$hMat
   ##### Save last in-sample h Value
   hLastInSamp=inSampleHMatObj$xTemp
-  
+
   ######Calculate vMat
   vMatESN=t(scaledInSampY)%*%t(inSampleHMat)%*%solve(inSampleHMat%*%t(inSampleHMat)+ridgeMat)
-  
-  
+
+
   ###### Calculate Out-of-Sample Forecasts
   uProdMatOutSamp=U%*%t(designMatrixOutSample)
   ##### Calculate Out-of-Sample hMat
   outSampleHMat=createHMatR(nh,nrow(designMatrixOutSample),wMatScaled,uProdMatOutSamp,hLastInSamp,quadInd)$hMat
-  
+
   #####Out-Sample Forecasts
   scaledForecast=vMatESN%*%outSampleHMat
   unScaledForecasts=(scaledForecast*inSampStDev+inSampMean)[,xMatchIndexes]
 
-  
+
   return(list(U=U,wMatScaled=wMatScaled,vMatESN=vMatESN,inSampleHMat=inSampleHMat,unScaledForecasts=unScaledForecasts))
 }
 
@@ -188,28 +186,28 @@ genResR=function(nh,wWidth,uWidth,piW,piU,nuESN,quadInd,DataObj,setParObj,testLe
 #' @export
 createHMatR=function(nh,numTimePerds,wMatScaled,uProdMat,startValues,quadInd){
   xTemp=startValues
-  
-  
-  
+
+
+
   if(quadInd){
     hMat = matrix(0,2*nh ,numTimePerds)
   }else{
     hMat = matrix(0,nh ,numTimePerds)
   }
-  
- 
-  
+
+
+
   for (t in 1:numTimePerds){
-    
+
     xTemp =tanh( wMatScaled %*% xTemp+uProdMat[,t] )
-    
-    ######allows for quadratic terms 
+
+    ######allows for quadratic terms
     if(quadInd){
       hMat[,t]=c(xTemp,xTemp^2)
     }else{
       hMat[,t] = c(xTemp)
     }
-    
+
   }
   return(list(hMat=hMat,xTemp=xTemp))
 }
@@ -221,19 +219,19 @@ createHMatR=function(nh,numTimePerds,wMatScaled,uProdMat,startValues,quadInd){
 #' @export
 findMonth=function(curPeriod){
   numMonth=curPeriod%%12
-  
+
   if(numMonth==0){
     curMonth="Dec."
   }
-  
+
   if(numMonth==1){
     curMonth="Jan."
   }
-  
+
   if(numMonth==2){
     curMonth="Feb."
   }
-  
+
   if(numMonth==3){
     curMonth="March"
   }
@@ -255,14 +253,14 @@ findMonth=function(curPeriod){
   if(numMonth==9){
     curMonth="Sept."
   }
-  
+
   if(numMonth==10){
     curMonth="Oct."
   }
   if(numMonth==11){
     curMonth="Nov."
   }
-  
+
   return(curMonth)
 }
 
@@ -273,13 +271,13 @@ findMonth=function(curPeriod){
 #' @export
 yrMonthLabel=function(curPeriod,curYr){
   numMonth=curPeriod%%12
-  
+
   curMontNum=ifelse(numMonth==0,12,numMonth)
-  
+
   tempLastDigit=round((curYr*.01-as.integer(curYr*.01))*100)
-  
+
   returnLabel=paste(curMontNum,"/",tempLastDigit, sep = "")
-  
+
   return(returnLabel)
 }
 
