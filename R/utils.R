211184c08ba5cde612c4952fcd2a5866bf0efaf7
andrewzm <andrewzm@gmail.com>
2020-12-15 08:05:52 +1100

    Disabled distR in STRbook... why was it there? Nothing to indicate it should be there

--- R/utils.R
+++ R/utils.R
@@ -29,37 +29,37 @@ fill_scale <- function(palette = "Spectral", name = "", limits = NULL) {
                          limits = limits)
 }
 
-#' @name dist-matrix
-#' @title Distance Matrix Computation from Two Matrices
-#' @description This function extends \code{dist} to accept two arguments.
-#' @param x1 matrix of size N1 x n
-#' @param x2 matrix of size N2 x n
-#' @details Computes the distances between the coordinates in \code{x1} and the coordinates in \code{x2}. The matrices \code{x1} and \code{x2} do not need to have the same number of rows, but need to have the same number of columns (dimensions).
-#' @return Matrix of size N1 x N2
-#' @export
-#' @examples
-#' A <- matrix(rnorm(50),5,10)
-#' D <- distR(A,A[-3,])
-distR <- function (x1, x2 = NULL)  {
-    ## Try to coerce to matrix
-    if (!is.matrix(x1)) {
-        x1 <- as.matrix(x1)
-    }
+## #' @name dist-matrix
+## #' @title Distance Matrix Computation from Two Matrices
+## #' @description This function extends \code{dist} to accept two arguments.
+## #' @param x1 matrix of size N1 x n
+## #' @param x2 matrix of size N2 x n
+## #' @details Computes the distances between the coordinates in \code{x1} and the coordinates in \code{x2}. The matrices \code{x1} and \code{x2} do not need to have the same number of rows, but need to have the same number of columns (dimensions).
+## #' @return Matrix of size N1 x N2
+## #' @export
+## #' @examples
+## #' A <- matrix(rnorm(50),5,10)
+## #' D <- distR(A,A[-3,])
+## distR <- function (x1, x2 = NULL)  {
+##     ## Try to coerce to matrix
+##     if (!is.matrix(x1)) {
+##         x1 <- as.matrix(x1)
+##     }
 
-    ## If x2 is not specified set it equatl to x1
-    if (is.null(x2)) {
-        x2 <- x1
-    }
+##     ## If x2 is not specified set it equatl to x1
+##     if (is.null(x2)) {
+##         x2 <- x1
+##     }
 
-    ## If it is specified, coerce it to matrix
-    if (!is.matrix(x2)) {
-        x2 <- as.matrix(x2)
-    }
+##     ## If it is specified, coerce it to matrix
+##     if (!is.matrix(x2)) {
+##         x2 <- as.matrix(x2)
+##     }
 
-    ## Basic check
-    if(!(ncol(x1) == ncol(x2)))
-        stop("x1 and x2 have to have same number of columns")
+##     ## Basic check
+##     if(!(ncol(x1) == ncol(x2)))
+##         stop("x1 and x2 have to have same number of columns")
 
-    ## Compute the distance in C (distR_C is a wrapper)
-    distR_C(x1,x2)
-}
+##     ## Compute the distance in C (distR_C is a wrapper)
+##     distR_C(x1,x2)
+## }
